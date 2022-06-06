ea11d35bc671e2a9208b59c881717dc18bfabbbf
TretyakovaA <putinvivo@bk.ru>
2022-06-06 20:38:54 +0300

    create main

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int age = 17;
+        if (age > =18){
+        System.out.println("Ты можешь водить");
+        }
+        else {
+            System.out.println("Ты не можешь водить");
+        }
+        int age = 19;
+
+    }
+}
